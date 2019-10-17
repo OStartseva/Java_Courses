a779b92968bce9f87ecf31ad94ae01003550ed17
OStartseva <olgastar888@gmail.com>
2019-10-17 22:35:04 +0300

    Task4

--- src/com/company/Task3.java
+++ src/com/company/Task3.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class HelloWorld {
+public class Task3 {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
---
+++ src/com/company/Task4.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String read = scanner.nextLine();
+        int num = Integer.parseInt(read, 2);
+        System.out.println(num);
+
+    }
+}
