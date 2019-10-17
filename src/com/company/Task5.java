d8c5d6e76aff1e485a2de7c5e910eb4c89f1660b
OStartseva <olgastar888@gmail.com>
2019-10-17 22:39:19 +0300

    Task5

---
+++ src/com/company/Task5.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        System.out.println(input * 1);
+        System.out.println(input * 2);
+        System.out.println(input * 3);
+        System.out.println(input * 4);
+        System.out.println(input * 5);
+        System.out.println(input * 6);
+        System.out.println(input * 7);
+        System.out.println(input * 8);
+        System.out.println(input * 9);
+        System.out.println(input * 10);
+
+    }
+}
