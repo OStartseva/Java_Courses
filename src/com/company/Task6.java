af1852f410dd36ee57ba154257c5cc767e8fbfef
OStartseva <olgastar888@gmail.com>
2019-10-22 23:27:03 +0300

    Task7 Исправление

--- src/com/company/Task6.java
+++ src/com/company/Task6.java
@@ -7,16 +7,18 @@ public class Task6 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Введите 3 числа:");
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int c = scanner.nextInt();
-        int d = (a + b + c) /3;
 
-        System.out.println(d);
+        double d = (a + b + c) /3.0;
+        System.out.println("Среднее арифметическое: " + d);
 
-        int e = d / 2 ;
 
+        int e = (int) d / 2 ;
+
+        System.out.println("Среднее арифметическое деленное на 2 без остатка: " + e);
         if (e > 3){
             System.out.println("Программа выполнена корректно");
         }
