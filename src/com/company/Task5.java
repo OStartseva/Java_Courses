eccf295bc6c0e5f2b9009dac484258aa4b5f4935
OStartseva <olgastar888@gmail.com>
2019-10-22 23:17:35 +0300

    Task5 Исправление

--- src/com/company/Task5.java
+++ src/com/company/Task5.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class Task5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число:");
         int input = scanner.nextInt();
 
+        System.out.println("Результат умножения:");
         System.out.println(input * 1);
         System.out.println(input * 2);
         System.out.println(input * 3);
