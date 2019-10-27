511857cab17ab58a6ab7e1dc2a7f5ab41be1b3de
OStartseva <olgastar888@gmail.com>
2019-10-27 20:05:37 +0300

    Task11

---
+++ src/com/company/Task11.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String string = scanner.nextLine();
+
+        System.out.println(string);
+    }
+}
