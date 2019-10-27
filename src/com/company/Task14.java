2ff729aa9eec72159e44c35c1a20acf2a1a485d7
OStartseva <olgastar888@gmail.com>
2019-10-27 20:35:20 +0300

    Task14

---
+++ src/com/company/Task14.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Task14 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        String s = scanner.nextLine();
+        System.out.println(s);
+
+        int x = Integer.parseInt(s);
+        System.out.println(x);
+
+        double y = x;
+        System.out.println(y);
+    }
+}
