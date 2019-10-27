eb29bb6a37a922eb54686fb25bbe2bb791ea6fd4
OStartseva <olgastar888@gmail.com>
2019-10-27 21:05:50 +0300

    Task16

---
+++ src/com/company/Task16.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Task16 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("/");
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNext()){
+            System.out.println(scanner.nextLine());
+        }
+        scanner.close();
+    }
+}
